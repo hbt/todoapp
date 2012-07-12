3b68dfbbd6baa56b51b3cafff543bb639d927aa1
hbt <hassenbentanfous@gmail.com>
2012-07-11 20:09:58 -0400

    all in one using requirejs

---
+++ app/build/build.js
@@ -0,0 +1,10 @@
+// node r.js -o build.js
+// disable urlArgs or use web version
+({
+    name: 'app',
+    baseUrl: '../web/assets/javascripts',
+    out: 'min.js',
+    optimize: "none",
+    findNestedDependencies: true,
+    mainConfigFile: '../web/assets/javascripts/app.js'
+})
